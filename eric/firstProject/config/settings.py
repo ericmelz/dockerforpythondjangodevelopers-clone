d2dfa62b1119740a52440cc236327459352e57f3
ericmelz <eric@emelz.com>
2024-04-18 14:48:49 -0700

    works

--- eric/firstProject/config/settings.py
+++ eric/firstProject/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-cgl&40+u^+$@h_mgsjquoi4eqa%-ajxg&^hz*o224staz4ph$k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
